644aa662c962a345be2eb4c9c9809fc2e05b3a53
FatmaElhosary <engfatmafathy@gmail.com>
2020-06-08 22:10:56 +0200

    made userprofile and eidited login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TeamComponent } from './team/team.component';
-=======
+
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { LoginComponent } from './login/login.component';
 
@@ -21,8 +21,8 @@ import { LoginComponent } from './login/login.component';
     HomeComponent,
     ContactComponent,
     NavbarComponent,
-    TeamComponent
-=======
+    TeamComponent,
+
     UserProfileComponent,
     LoginComponent
 
