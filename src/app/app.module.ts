50d33032a32849b5627a6617eda7d8421e757ae0
aml ragab <amlragab81@gmail.com>
2020-06-08 05:31:22 +0200

    adding my branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import {FormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    ResetPasswordComponent
   ],
   imports: [
     BrowserModule,
