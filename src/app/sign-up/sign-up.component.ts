26edcf5c5286e77c0a5cfc245619d9db15c3834b
aml ragab <amlragab81@gmail.com>
2020-06-15 14:03:04 +0200

    adding validation

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl,FormGroup,Validators } from '@angular/forms';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
@@ -7,10 +7,22 @@ import { FormControl } from '@angular/forms';
 })
 export class SignUpComponent implements OnInit {
 
+
+  signupForm=new FormGroup({
+    'first-name':new FormControl(null,[Validators.required,Validators.maxLength(8),Validators.minLength(3)]),
+    'last-name':new FormControl(null,[Validators.required,Validators.maxLength(8),Validators.minLength(3)]),
+    'email':new FormControl(null,[Validators.required,Validators.email]),
+    'password':new FormControl(null,[Validators.required,Validators.pattern(/^[A-Z][0-9]{2,8}$/)]),
+    'repassword':new FormControl(null,[Validators.required,])
+
+
+
+
+  })
   constructor() { }
 
   ngOnInit(): void {
   }
-  SignUpComponent = new FormControl('');
+
 
 }
