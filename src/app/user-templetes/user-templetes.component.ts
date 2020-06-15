168ce2e076c28472c2c5a31e2d9de5fb6e1f4c10
FatmaElhosary <engfatmafathy@gmail.com>
2020-06-15 13:29:29 +0200

    edit navbar to show profile pic& login & profile settings

--- src/app/user-templetes/user-templetes.component.ts
+++ src/app/user-templetes/user-templetes.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+class UserTempletes{
+name:string;
+id:number;
+}
 @Component({
   selector: 'app-user-templetes',
   templateUrl: './user-templetes.component.html',
@@ -9,6 +12,17 @@ export class UserTempletesComponent implements OnInit {
   events: string[] = [];
   opened: boolean;
   constructor() {}
+  userTempletes:Array<UserTempletes>=[];
+ 
+ 
+  setTempletesvalues(){
+   let templete:UserTempletes=new UserTempletes();
+ templete.name="blog";
+ templete.id=1;
+ this.userTempletes.push(templete);
+  }
+  ngOnInit(): void {
+    this.setTempletesvalues();
 
-  ngOnInit(): void {}
+  }
 }
