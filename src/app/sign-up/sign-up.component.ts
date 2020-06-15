765ac98501798fd092c738d122897983154adde1
aml ragab <amlragab81@gmail.com>
2020-06-15 16:26:46 +0200

    final validation

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -8,17 +8,22 @@ import { FormControl,FormGroup,Validators } from '@angular/forms';
 export class SignUpComponent implements OnInit {
 
 
-  signupForm=new FormGroup({
-    'first-name':new FormControl(null,[Validators.required,Validators.maxLength(8),Validators.minLength(3)]),
-    'last-name':new FormControl(null,[Validators.required,Validators.maxLength(8),Validators.minLength(3)]),
+  siginupForm=new FormGroup({
+    'first':new FormControl(null,[Validators.required,Validators.maxLength(8),Validators.minLength(3)]),
+    'last':new FormControl(null,[Validators.required,Validators.maxLength(8),Validators.minLength(3)]),
     'email':new FormControl(null,[Validators.required,Validators.email]),
-    'password':new FormControl(null,[Validators.required,Validators.pattern(/^[A-Z][0-9]{2,8}$/)]),
-    'repassword':new FormControl(null,[Validators.required,])
+    'password':new FormControl(null,[Validators.required,Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/)]),
+    'repassword':new FormControl(null,[Validators.required])
 
 
 
 
   })
+  onSubmit() {
+ 
+    // TODO: Use EventEmitter with form value
+    console.warn(this.siginupForm.value);
+  }
   constructor() { }
 
   ngOnInit(): void {
